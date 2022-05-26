7cd646f6ebe03ed59d43a323b85f313f9d41ff2f
Abdelrahman <94563355+Abdelrhamna@users.noreply.github.com>
2022-05-26 20:13:46 +0200

    Add files via upload

---
+++ assets/js/animate.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $(".Lobster").mouseenter(function(){
+        $(".s-Lobster,.s-Lobster *").fadeToggle(1000);
+    });
+    $(".Cake").mouseenter(function(){
+        $(".s-Cake,.s-Cake *").fadeToggle(1000);
+    });
+    $(".bread").mouseenter(function(){
+        $(".s-bread,.s-bread *").fadeToggle(1000);
+    });
+    $(".greek").mouseenter(function(){
+        $(".s-greek,.s-greek *").fadeToggle(1000);
+    });
+    $(".mozzarella").mouseenter(function(){
+        $(".s-mozzarella,.s-mozzarella *").fadeToggle(1000);
+    });
+    $(".spinach").mouseenter(function(){
+        $(".s-spinach,.s-spinach *").fadeToggle(1000);
+    });
+});
